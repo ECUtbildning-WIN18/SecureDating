f7c210304133eacc80941e495fe0d53268b27917
Stefan Pettersson <trancedude79@hotmail.com>
2018-09-19 12:54:24 +0200

    spelling

--- securedating/securedating/User.cs
+++ securedating/securedating/User.cs
@@ -6,15 +6,15 @@ namespace securedating
 {
     class User
     {
-        public string FristName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthdate { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
 
-        public User(string fristName, string lastName, DateTime birthdate, string userName, string password)
+        public User(string firstName, string lastName, DateTime birthdate, string userName, string password)
         {
-            FristName = fristName;
+            FirstName = firstName;
             LastName = lastName;
             Birthdate = birthdate;
             UserName = userName;
