56bc9408a7e6399ccf4c54482ad45a5c6173c688
Philip Englund <phille96@gmail.com>
2018-09-20 13:18:53 +0200

    fixed conflicts

--- securedating/securedating/User.cs
+++ securedating/securedating/User.cs
@@ -7,33 +7,21 @@ namespace securedating
 {
     class User
     {
-<<<<<<< HEAD
+
         public Profile Profile { get; set; }
 
         public string FirstName { get; internal set; }
 
         public string LastName { get; internal set; }
 
-=======
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
->>>>>>> develop
         public DateTime Birthdate { get; set; }
 
-<<<<<<< HEAD
         private string UserName { get; }
 
         private string Password { get; }
 
         public User(string userName, string password)
         {
-=======
-        public User(string firstName, string lastName, DateTime birthdate, string userName, string password)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Birthdate = birthdate;
->>>>>>> develop
             UserName = userName;
             Password = password;
             Profile = new Profile(userName);
